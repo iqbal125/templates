8a71da24fe3513a7e77cf76394fa8455762bdc66
iqbal125 <iqbal125sf@gmail.com>
2025-12-06 11:36:41 -0800

    remove zustand template

--- react-state/src/pages/Todo/types.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const todoSchema = z.object({
-    text: z.string().min(1, 'Todo text is required').max(200, 'Todo text is too long'),
-});
-
-export type TodoFormData = z.infer<typeof todoSchema>;
--- react-state/src/types/readme.md
+++
@@ -1,1 +0,0 @@
-## Keep Global types here
